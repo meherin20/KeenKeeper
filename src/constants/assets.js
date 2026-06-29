14c941f24c6b4af2cf60e3eef0e22bd5d454ea54
meherin sohani <msohani2330420@bscse.uiu.ac.bd>
2026-06-29 14:20:38 +0600

    asset.js

---
+++ src/constants/assets.js
@@ -0,0 +1,28 @@
+export const ASSETS = {
+  logo: '/assets/logo.png',
+  logoXl: '/assets/logo-xl.png',
+  home: '/assets/home.svg',
+  timeline: '/assets/timeline.svg',
+  stats: '/assets/stats.svg',
+  call: '/assets/call.png',
+  text: '/assets/text.png',
+  video: '/assets/video.png',
+  meetup: '/assets/meetup.png',
+  instagram: '/assets/instagram.png',
+  facebook: '/assets/facebook.png',
+  twitter: '/assets/twitter.png',
+};
+
+export const CHECK_IN_ICONS = {
+  Call: ASSETS.call,
+  Text: ASSETS.text,
+  Video: ASSETS.video,
+};
+
+export const TIMELINE_ICONS = {
+  call: ASSETS.call,
+  text: ASSETS.text,
+  video: ASSETS.video,
+  meetup: ASSETS.meetup,
+};
+
